5251412a4e0b5d5047f7afde3aefe2b5d984588a
田原 一樹 <ssp0727.lnc@gmail.com>
2019-10-17 22:41:32 +0900

    fix bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,40 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { saveAs } from 'file-saver'
+import React, { Component } from 'react';
+import axios from 'axios';
+import { saveAs } from 'file-saver';
+
+import './App.css';
 
 class App extends Component {
   state = {
     name: '',
-    receipId: 0,
+    receiptId: 0,
     price1: 0,
-    price2: 0
+    price2: 0,
   }
 
-  handleChange = ({ target: { value, name }}) => this.setState({[name]: value})
+  handleChange = ({ target: { value, name }}) => this.setState({ [name]: value })
 
   createAndDownloadPdf = () => {
     axios.post('/create-pdf', this.state)
-      .then(()=> axios.get('fetch-pdf', { responseType: 'blob '}))
+      .then(() => axios.get('fetch-pdf', { responseType: 'blob' }))
       .then((res) => {
-        const pdfBlob = new Blob([res.data], { type: 'application/pdf' })
+        const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
 
-        saveAs(pdfBlob, 'newPdf.pdf')
+        saveAs(pdfBlob, 'newPdf.pdf');
       })
   }
+
   render() {
     return (
       <div className="App">
-        <input type="text" placeholder="お客様名を入れてください" name="name" onChange={this.handleChange}/>
-        <input type="number" placeholder="Receipt ID" name="receipId" onChange={this.handleChange}/>
-        <input type="number" placeholder="price1" name="price1" onChange={this.handleChange}/>
-        <input type="number" placeholder="price2" name="price2" onChange={this.handleChange}/>
+        <input type="text" placeholder="Name" name="name" onChange={this.handleChange}/>
+        <input type="number" placeholder="Receipt ID" name="receiptId" onChange={this.handleChange} />
+        <input type="number" placeholder="Price 1" name="price1" onChange={this.handleChange} />
+        <input type="number" placeholder="Price 2" name="price2" onChange={this.handleChange} />
         <button onClick={this.createAndDownloadPdf}>Download PDF</button>
       </div>
-    ) 
+    );
   }
 }
 
-export default App
+export default App;
